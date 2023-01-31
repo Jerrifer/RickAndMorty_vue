ae4ab6858a52fd3f80277652d883fb00b2620aeb
unknown <efurbano@misena.edu.com>
2023-01-31 15:42:57 -0500

    first commit

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="character">
+        <div class="character__info">
+            {{ character.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    prop: ['character']
+}
+</script>
